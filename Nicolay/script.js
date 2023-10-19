f71b4f3d98646c85093dae1ce02afddf5a5308ac
nicolovli <145984680+nicolovli@users.noreply.github.com>
2023-10-19 11:23:24 +0200

    s

--- Nicolay/script.js
+++ Nicolay/script.js
@@ -7,37 +7,30 @@ function StartButton () {
 }
 
 const bak = document.getElementsByClassName("bak"); //henter ut alt med class "bak" og lagrer det i en variabel
+const foran = document.getElementsByClassName("foran"); //henter ut alt med class "foran";
+const kort = document.getElementsByClassName("kort"); //henter ut alt med class "kort";
 
 let antallKlikk = 0;
 
 for (const bakside of bak) { //dette er en for of loop som kjører gjennom hvert element med class "bak". 
     bakside.addEventListener("click", function() { //her hører programmet etter et "click" på en av bildene med class "bak"
       bakside.parentElement.classList.toggle("flipped"); //når den hører et klikk setter den en css style som heter "flipped" til true
-      antallKlikk++;
-      if (antallKlikk % 2 == 0) {  //i denne funksjonen prøver jeg å få kortene til å flippe tilbake til baksiden sin når baksidene har blitt trykket på partall antall ganger
-        let test = document.querySelectorAll('.bakside');
-        test.forEach(element => {
-          element.style.opacity = '1'; 
-        });
-              console.log(antallKlikk);
-      }
+      // antallKlikk++;
+       //if (test) {  //i denne funksjonen prøver jeg å få kortene til å flippe tilbake til baksiden sin når baksidene har blitt trykket på partall antall ganger
+        
+      // }
     });
   };
 
+// dette er funksjon for å matche kort med hverandre
+  
 
-
-
-
-
-let firstCard, secondCard;
-
-for (const bakside of bak) {
-  bakside.addEventListener("click", function(){
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-      console.log("match");
-    };
+for (const kortside of kort) {
+  kortside.addEventListener("click", function () {
+    const clickedCardValue = kortside.dataset.framework;
+    console.log("Clicked card value forside: " + clickedCardValue);
   });
-};
+}
 
 shuffleCards();
 
@@ -47,22 +40,7 @@ function shuffleCards() {
   kort.forEach(kort => {
       var randomPos = Math.floor(Math.random() * 16);
       kort.style.order = randomPos;
-      console.log = randomPos
   });
 }
 
-
-
-
 });
-
-//Denne funksjonen sjekker om kortene matcher hverandre
-function kortMatcher() {
-  var kort = document.querySelectorAll('.kort');
-  if (kort.dataframework === kort.dataframework && "flipped" === true) {
-    console.log = "Kortet matcher";
-  }
-  else {
-    console.log = "Kortet matcher ikke";
-  }
-}
