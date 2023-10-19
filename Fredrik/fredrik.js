20db4731c3a6aa99247538854eab27818aa3ddbd
Fredrik <fredrik363@gmail.com>
2023-10-19 09:50:27 +0200

    Kopierte koden til min egen side

---
+++ Fredrik/fredrik.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+  
+
+    function StartButton () {
+        
+    }
+    
+    const bak = document.getElementsByClassName("bak"); //henter ut alt med class "bak" og lagrer det i en variabel
+    
+    for (const bakside of bak) { //dette er en for of loop som kjører gjennom hvert element med class "bak". 
+        bakside.addEventListener("click", function() { //her hører programmet etter et "click" på en av bildene med class "bak"
+          bakside.parentElement.classList.toggle("flipped"); //når den hører et klikk setter den en css style som heter "flipped" til true
+        });
+      };
+    
+    let antallKlikk = 0;
+    
+    //i denne funksjonen prøver jeg å få kortene til å flippe tilbake til baksiden sin når baksidene har blitt trykket på partall antall ganger
+    
+    
+    for (const flipp of bak) {
+        flipp.addEventListener("click", function() {
+            antallKlikk++;
+            if (antallKlikk % 2 == 0) {
+                blablablabla
+                
+            }
+        });
+    };
+    
+    
+    
+    
+    
+    });
+    
