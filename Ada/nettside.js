ad81e1c44be17f12344dc3bc8ec832bd5ab510bd
adabjork <adabm@stud.ntnu.no>
2023-10-19 12:08:48 +0200

    La inn regler, la til jquery, osv

--- Ada/nettside.js
+++ Ada/nettside.js
@@ -1,4 +1,40 @@
 $(document).ready(function(){
+    $("#hovedoverskrift").css("opacity", 1);
+        $("#hovedoverskrift").mouseenter(function() {
+
+            $(this).animate({opacity: 0.6});
+        });
+            $("#hovedoverskrift").mouseleave(function(){
+            $(this).animate({opacity: 1});
+        });
+
+
+    $(document).ready(function(){
+    $(".navbar-brand").css("opacity", 1);
+        $(".navbar-brand").mouseenter(function() {
+
+            $(this).animate({opacity: 0.4});
+        });
+            $(".navbar-brand").mouseleave(function(){
+            $(this).animate({opacity: 1});
+        });
+    
+
+    });
+    $(document).ready(function(){
+    $(".nav-item").css("opacity", 1);
+        $(".nav-item").mouseenter(function() {
+
+            $(this).animate({opacity: 0.6});
+        });
+            $(".nav-item").mouseleave(function(){
+            $(this).animate({opacity: 1});
+        });
+    
+    });
+    
+
+$(document).ready(function(){
     $("#kortbilde").css("opacity", 0.5);
         $("#kortbilde").mouseenter(function() {
 
@@ -9,3 +45,4 @@ $(document).ready(function(){
         });
     
     });
+});
