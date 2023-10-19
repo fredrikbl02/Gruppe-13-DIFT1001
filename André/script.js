f8e64a542fd669ae43888c740b6e1d3ff9684855
askmind <andrekarsten13@gmail.com>
2023-10-19 10:21:29 +0200

    update

--- André/script.js
+++ André/script.js
@@ -8,26 +8,35 @@ function StartButton () {
 
 const bak = document.getElementsByClassName("bak"); //henter ut alt med class "bak" og lagrer det i en variabel
 
+let antallKlikk = 0;
+
 for (const bakside of bak) { //dette er en for of loop som kjører gjennom hvert element med class "bak". 
     bakside.addEventListener("click", function() { //her hører programmet etter et "click" på en av bildene med class "bak"
       bakside.parentElement.classList.toggle("flipped"); //når den hører et klikk setter den en css style som heter "flipped" til true
+      antallKlikk++;
+      if (antallKlikk % 2 == 0) {
+        let test = document.querySelectorAll('.foran');
+              test.style.opacity = '100';
+              console.log(antallKlikk);
+      }
     });
   };
 
-let antallKlikk = 0;
+
 
 //i denne funksjonen prøver jeg å få kortene til å flippe tilbake til baksiden sin når baksidene har blitt trykket på partall antall ganger
 
 
-for (const flipp of bak) {
-    flipp.addEventListener("click", function() {
-        antallKlikk++;
-        if (antallKlikk % 2 == 0) {
-            blablablabla
-            
-        }
-    });
+let firstCard, secondCard;
+
+for (const bakside of bak) {
+  bakside.addEventListener("click", function(){
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      console.log("match");
+    };
+  });
 };
+
 
 
 
