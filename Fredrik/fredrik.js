9155eb941f1b3ea39c422e247c7cca45854744cf
Fredrik <fredrik363@gmail.com>
2023-10-19 13:13:15 +0200

    Min side

--- Fredrik/fredrik.js
+++ Fredrik/fredrik.js
@@ -1,73 +1,77 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
-  
+  const bak = document.getElementsByClassName("bak");
+  const kort = document.getElementsByClassName("kort");
+  let firstCard = null;
+  let secondCard = null;
 
-  function StartButton () {
-      
+  for (const bakside of bak) {
+    bakside.addEventListener("click", function () {
+      bakside.parentElement.classList.toggle("flipped");
+    });
   }
-  
-  const bak = document.getElementsByClassName("bak"); //henter ut alt med class "bak" og lagrer det i en variabel
-  
-  for (const bakside of bak) { //dette er en for of loop som kjører gjennom hvert element med class "bak". 
-      bakside.addEventListener("click", function() { //her hører programmet etter et "click" på en av bildene med class "bak"
-        bakside.parentElement.classList.toggle("flipped"); //når den hører et klikk setter den en css style som heter "flipped" til true
-      });
-    };
-  
-  let antallKlikk = 0;
-  
-  //i denne funksjonen prøver jeg å få kortene til å flippe tilbake til baksiden sin når baksidene har blitt trykket på partall antall ganger
-  
-  
-  for (const flipp of bak) {
-      flipp.addEventListener("click", function() {
-          antallKlikk++;
-          if (antallKlikk % 2 == 0) {
-              blablablabla
-              
-          }
-      });
-  };
-  
-  });
-  
-  
- 
-  let matchesFound = 0;
+
+  for (const kortside of kort) {
+    kortside.addEventListener("click", function () {
+      if (!firstCard) {
+        firstCard = kortside;
+        firstCard.classList.add("flipped");
+      } else if (!secondCard) {
+        secondCard = kortside;
+        secondCard.classList.add("flipped");
+
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+          console.log("Match!");
+          firstCard.classList.add("matched");
+          secondCard.classList.add("matched");
+          firstCard = null;
+          secondCard = null;
+        } else {
+          console.log("No match!");
+          setTimeout(() => {
+            firstCard.classList.remove("flipped");
+            secondCard.classList.remove("flipped");
+            firstCard = null;
+            secondCard = null;
+          }, 1000);
+        }
+      }
+    });
+  }
+
+  function shuffleCards() {
+    var kort = document.querySelectorAll('.kort');
+    kort.forEach(kort => {
+      var randomPos = Math.floor(Math.random() * 16);
+      kort.style.order = randomPos;
+    });
+  }
+
+  shuffleCards();
+
   let timerSeconds = 0;
   let timerInterval;
-  
+
   function updateTimer() {
     timerSeconds++;
     document.getElementById("timer").textContent = "Time: " + timerSeconds + " seconds";
   }
-  
+
   function startTimer() {
     timerSeconds = 0;
     document.getElementById("timer").textContent = "Time: 0 seconds";
     timerInterval = setInterval(updateTimer, 1000);
   }
-  
-  function stopTimer() {
+
+  function resetTimer() {
     clearInterval(timerInterval);
-    document.getElementById("timer").textContent = "Time: " + timerSeconds + " seconds";
-   
+    document.getElementById("timer").textContent = "Time: 0 seconds";
   }
 
-  
-  // Your matching logic goes here
-  for (let i = 1; i <= 100; i++) { // Replace 100 with your actual limit
-    // Your matching logic here
-    if (i % 7 === 0) { // Example matching condition (replace with your actual condition)
-      matchesFound++;
-      console.log("Match found!");
-  
-      if (matchesFound === 8) {
-        console.log("8 matches found. Stopping the timer.");
-        stopTimer(); // Stop the timer when 8 matches are found
-        break; // Exit the loop
-      }
-    }
+  // The StartButton function to start the game and timer
+  function StartButton() {
+    startTimer(); // Start the timer when the button is pressed
   }
-  
-  console.log("Loop finished.");
+
+  // Add an event listener for the "Reset" button or other game functionality
+  document.getElementById("resetButton").addEventListener("click", resetTimer);
+});
