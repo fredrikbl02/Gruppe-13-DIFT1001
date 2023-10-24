3fc9d0afa3ca60dd5a845ee83d5a6c26c42d1bf5
ErikBrorby <erik.brorby@gmail.com>
2023-10-24 15:06:12 +0200

    Laget en timer som har blitt lagt i timer.js

--- Erik/erik.js
+++ Erik/erik.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function () {
   
     const bak = document.getElementsByClassName("bak"); //henter ut alt med class "bak" og lagrer det i en variabel
@@ -80,31 +81,52 @@ document.addEventListener("DOMContentLoaded", function () {
     
     });
     
-    let timerSeconds = 0;
-    let timerInterval;
-    
-    function updateTimer() {
-      timerSeconds ++;
-      document.getElementById("timer").textContent = "Time: " + timerSeconds + " seconds";
-    }
-    
-    function startTimer() {
-      timerSeconds = 0;
-      document.getElementById("timer").textContent = "Time: 0 seconds";
-      timerInterval = setInterval(updateTimer, 1000);
-    }
-    
-    function resetTimer() {
-      clearInterval(timerInterval);
-      document.getElementById("timer").textContent = "Time: 0 seconds";
-    }
-    
-    // The StartButton function to start the game and timer
+
     function StartButton() {
-      startTimer(); // Start the timer when the button is pressed
+
+      let time = document.getElementById("time");
+      let seconds = 0;
+      let tideler = 0;
+      time.innerText = formatTime(seconds, tideler);
+
+
+
+
+      //Timer for tideler og sekunder
+      setInterval(() => {
+        tideler++; //Tideler legges til (inkrement)
+        if(tideler===10){ //Når tideler når 10 resettes tideler til 0 og sekunder inkremeres (øker med 1)
+        tideler = 0;
+        seconds++;
+        }
+        time.innerText = formatTime(seconds, tideler);
+
+      }, 100); // legges til hvert tidel av et sekund
+
+      
+
+      //Denne funksjonen gjør at dersom sekunder overstiger 60 blir det omgjort til minutter
+      function formatTime(seconds, tideler){
+        let minutes = Math.floor(seconds/60); //Beregner antall minutter ved å ta heltall sekunder / 60, dvs -->
+        //80 sekunder blir fortsatt 60=1 minutt fordi det er nærmeste hele tall
+        let remainingSeconds = seconds % 60; //Beregner antall sekunder det er igjen ved hjelp av modulus
+        //F.eks. 200%60 blir 20 fordi 60*3= 180, 20 i rest. Dette betyr 3 minutter og 20 sekunder
+        let timer = minutes + " " + "minutt" + " " + remainingSeconds + " " + "sekunder" + " " + tideler + " " + "tideler"; //Skriver ut
+        return timer; //Skriver ut
+      }
+
+
+
+      //Denne brukes for å stoppe timeren, dette vet vi ikke ennå
+    //   setTimeout(() => { 
+    //   location.reload(); //Brukes for å starte siten på nytt, trenger kanskje ikke denne senere.
+    //  }, 15000); //15000 her er ms til timeren stopper, vi trenger -->
+    // noe å kalle på når timeren skal stoppe, ikke laget ennå.
+
+    
+    
+ 
     }
     
-    // Add an event listener for the "Reset" button or other game functionality
-    document.getElementById("resetButton").addEventListener("click", resetTimer);
     
     
