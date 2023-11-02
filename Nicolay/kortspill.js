aa731a20efec270910c4e70f787389e1d20a7772
nicolovli <145984680+nicolovli@users.noreply.github.com>
2023-11-02 15:03:23 +0100

    kopierte over til Nicolay

--- Nicolay/kortspill.js
+++ Nicolay/kortspill.js
@@ -5,15 +5,61 @@ document.addEventListener("DOMContentLoaded", function () {
     const kort = document.getElementsByClassName("kort"); //henter ut alt med class "kort";
 
 
+    let seconds = 0;
+    let tideler = 0;
+    let timer = null;
+     //Timer
+
+      // Denne funksjonen gjør at dersom sekunder overstiger 60 blir det omgjort til minutter
+      function formatTime(seconds, tideler) {
+        let minutes = Math.floor(seconds / 60); // Beregner antall minutter ved å ta heltall sekunder / 60
+        let remainingSeconds = seconds % 60; // Beregner antall sekunder det er igjen ved hjelp av modulus
+        let formattedSeconds = remainingSeconds.toString().padStart(2, '0');
+        let timer = minutes + ":" + formattedSeconds + ":" + tideler;
+        return timer;
+      }
+
+     function startTimer () {
+      const section = document.getElementById("section");
+      let functionCalled = false; // Flag to track if the function has been called
+      let time = document.getElementById("time");
+      time.innerText = formatTime(seconds, tideler);
+ 
+      section.addEventListener("click", function () {
+        if (!functionCalled) {
+  
+          // Timer for tideler og sekunder
+          timer = setInterval(() => {
+            tideler++; // Tideler legges til (inkrement)
+            if (tideler === 10) { // Når tideler når 10 resettes tideler til 0 og sekunder inkremeres (øker med 1)
+              tideler = 0;
+              seconds++;
+            }
+            time.innerText = formatTime(seconds, tideler);
+          }, 100); // legges til hvert tidel av et sekund
+  
+          console.log("Function has been called");
+          functionCalled = true;
+  
+          // Set the flag to true to prevent the function from being called again
+          functionCalled = true;
+          }});
+ 
+
+    
+    };
    
     
     
-    
+    function bakEventListener() {
     for (const bakside of bak) { //dette er en for of loop som kjører gjennom hvert element med class "bak". 
         bakside.addEventListener("click", function() { //her hører programmet etter et "click" på en av bildene med class "bak"
           bakside.parentElement.classList.toggle("flipped"); //når den hører et klikk setter den en css style som heter "flipped" til true
         });
       };
+    };
+
+    bakEventListener();
     
     // dette er funksjon for å matche kort med hverandre
     
@@ -38,18 +84,16 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log("Match!");
             //teller opp antall matcher
         
-            if (count < 1) {
+            if (count < 8) {
                 count++;
             };
 
             if (count === 1) {
-                clearInterval(timer);
-                setTimeout(() => {
-                displayHighscores();
-                saveHighscores(highscores, navnArray);
-              }, 800);
-            };
-            
+              clearInterval(timer);
+              setTimeout(() => {
+              displayHighscores();
+            }, 800);
+          };
 
             // If it's a match, remove the click event listener to lock the matched cards
             firstCard.classList.add('matched');
@@ -94,107 +138,57 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
 
-    
-     //Timer
-
-     const section = document.getElementById("section");
-     let functionCalled = false; // Flag to track if the function has been called
-     let time = document.getElementById("time");
-     let seconds = 0;
-     let tideler = 0;
-     time.innerText = formatTime(seconds, tideler);
-
-     section.addEventListener("click", function () {
-       if (!functionCalled) {
- 
-         // Timer for tideler og sekunder
-         timer = setInterval(() => {
-           tideler++; // Tideler legges til (inkrement)
-           if (tideler === 10) { // Når tideler når 10 resettes tideler til 0 og sekunder inkremeres (øker med 1)
-             tideler = 0;
-             seconds++;
-           }
-           time.innerText = formatTime(seconds, tideler);
-         }, 100); // legges til hvert tidel av et sekund
- 
-         console.log("Function has been called");
-         functionCalled = true;
- 
-         // Set the flag to true to prevent the function from being called again
-         functionCalled = true;
-         }});
-
-    // Denne funksjonen gjør at dersom sekunder overstiger 60 blir det omgjort til minutter
-    function formatTime(seconds, tideler) {
-      let minutes = Math.floor(seconds / 60); // Beregner antall minutter ved å ta heltall sekunder / 60
-      let remainingSeconds = seconds % 60; // Beregner antall sekunder det er igjen ved hjelp av modulus
-      let formattedSeconds = remainingSeconds.toString().padStart(2, '0');
-      let timer = minutes + ":" + formattedSeconds + ":" + tideler;
-      return timer;
-    }
 
 
-    function displayHighscores() {
-      console.log("suksess");
-      let tid = formatTime(seconds, tideler);
-      let highscores = [];
-      let navnArray = [];
+    startTimer();
 
-      let navn = prompt("Gratulerer! Skriv inn navnet ditt her: ");
+// highscores
 
-          highscores.push(tid);
-          navnArray.push(navn);
-          highscores = highscores.slice(0, 5);
-  
-          let highScoresList = document.getElementById("highscore");
-  
-          highscores.forEach((tid) => {
-              let listItem = document.createElement("li");
-              listItem.innerText = navn + ": " + tid;
-              highScoresList.appendChild(listItem);
-    
-              
-          });
-      }
+function displayHighscores() {
+  console.log("suksess");
+  let tid = formatTime(seconds, tideler);
+  let highscores = [];
+  let navnArray = [];
+
+  let navn = prompt("Gratulerer! Skriv inn navnet ditt her: ");
+
+      highscores.push(tid);
+      navnArray.push(navn);
+      highscores = highscores.slice(0, 5);
+
+      let highScoresList = document.getElementById("highscore");
+
+      highscores.forEach((tid) => {
+          let listItem = document.createElement("li");
+          listItem.innerText = navn + ": " + tid;
+          highScoresList.appendChild(listItem);
+
+          
+      });
   }
-    );
 
-    // Save highscores to localStorage
-function saveHighscores(highscores, navnArray) {
-  localStorage.setItem('highscores', JSON.stringify(highscores));
-  localStorage.setItem('navnArray', JSON.stringify(navnArray));
-}
 
-// Load highscores from localStorage
-function loadHighscores() {
-  let highscores = localStorage.getItem('highscores');
-  return highscores ? JSON.parse(highscores) : [];
+  document.getElementById("newGame").onclick = () => {      
+    for (const kortside of kort) {
+      kortside.classList.remove('flipped', 'matched');
+    };
+    shuffleCards();
+    time.innerText = formatTime(0, 0);
+    count = 0;
+    
+    if (timer !== null) {
+      clearInterval(timer);
+  }
+    seconds = 0;
+    tideler = 0;
+    startTimer();
+ };
 
-  let navnArray = localStorage.getItem('navnArray');
-  return navnArray ? JSON.parse(navnArray) : [];
-}
+});
 
-// // Restart the game
-// function restartGame() {
-//   // Reset the game state
-//   firstCard = null;
-//   secondCard = null;
-//   isFlipping = false;
-//   count = 0;
-// }
-
-//   // Reset the UI elements
-//   for (const kortside of kort) {
-//     kortside.classList.remove('flipped', 'matched');
-//   }
-
-//   document.getElementById("newGame").onclick = () => {
-//     console.log("new game");
-//     saveHighscores(highscores, navnArray);
-//     restartGame();
-//   }
 
     
 
+    
     
       
